0a73a09644cc74be96679a130625cd9d512076ec
João Mário <marioartec39@gmail.com>
2024-10-08 18:37:48 -0300

    RateLimit.kt removed

--- src/main/kotlin/com/sanisamoj/plugins/RateLimit.kt
+++ src/main/kotlin/com/sanisamoj/plugins/RateLimit.kt
@@ -3,12 +3,9 @@ package com.sanisamoj.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.ratelimit.RateLimit
-import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRateLimit() {
     install(RateLimit) {
-        register {
-            rateLimiter(limit = 4, refillPeriod = 1.seconds)
-        }
+
     }
 }
